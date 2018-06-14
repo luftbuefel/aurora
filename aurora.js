5c657539a5cffb9c7d04df2aea054ca5686d70bd
Charles B <polyglob@yahoo.com>
2018-06-14 19:52:40 +0000

    trying to add tint changing

--- aurora.js
+++ aurora.js
@@ -5,8 +5,8 @@ let testCanvas = document.querySelector("#testCanvas");
 let testContext = testCanvas.getContext("2d");
 const FADE_AMOUNT = 3;
 
-// let offScreen = new OffscreenCanvas(canvas.width, canvas.height);
-// let offContext = offScreen.getContext("2d");
+// var offscreen = new OffscreenCanvas(256, 256);
+// var gl = offscreen.getContext('2d');
 
 let nightSky = new Image();
 nightSky.src = 'images/night.jpg';
@@ -16,8 +16,8 @@ trees.src="images/trees.png"
 
 let light = new Image();
 light.src="images/light.png";
+light.setAttribute("style","background-color:#00ff00ff;");
 
-let lastScreen = new Image();
 
 function bkgdLoadComplete(){
     context.drawImage(nightSky,0,0);
@@ -25,44 +25,55 @@ function bkgdLoadComplete(){
 }
 
 function update(){
-    fadeOut();//testContext.getImageData(0,0,testContext.width, testContext.height));
+    updateAuroraEffect();
+    context.drawImage(nightSky,0,0);
+    context.drawImage(testCanvas,0,0);
+    context.drawImage(trees,0,0);
     window.requestAnimationFrame(update);
 }
 
 function moveLight(e){
-    context.drawImage(nightSky,0,0);
-   // context.drawImage(light,e.clientX-(light.width/2),e.clientY-(light.height));
     testContext.drawImage(light,e.clientX-(light.width/2),e.clientY-(light.height));
-    //et fadeImage = testContext.getImageData(0,0,testCanvas.width, testCanvas.height);
-    context.drawImage(testCanvas,0,0);
-    //testContext.drawImage(trees,0,0);
-    context.drawImage(trees,0,0);
+    
+    
+    
+//     testContext.fillStyle = "#00ff0011";
+//   testContext.fillRect(0, 0, testCanvas.width, testCanvas.height)
+//   testContext.globalCompositeOperation = 'destination-atop'
+
+
+//   ctx.globalAlpha = 1
+//   testContext.drawImage(light, 0, 0)
+    //change the light's color
+    //light.setAttribute("style", "color: tint(#BADA55, 42%);");
+    
+   /*// Now we'll multiply a rectangle of your chosen color
+    testContext.fillStyle = '#00ff00';
+    testContext.globalCompositeOperation = 'source-out';
+    testContext.fillRect(0, 0, testCanvas.width, testCanvas.height);*/
+    
+    //console.log(light);
 }
 
 function displayData(e){
     console.log(testContext.getImageData(0,0,canvas.width, canvas.height));
 }
 
-function fadeOut(){
+function updateAuroraEffect(){
     if(testCanvas.width>0){
         let tempImage = testContext.getImageData(0,0,testCanvas.width, testCanvas.height);
         let tempData = tempImage.data;
         //reduce the alpha values; i.e. every fourth value
+        // for(let i = 3;i<tempData.length;i+=4){
         for(let i = 3;i<tempData.length;i+=4){
             tempData[i]=tempData[i]>FADE_AMOUNT?tempData[i]-FADE_AMOUNT:0;
+            tempData[i-2]+=FADE_AMOUNT;
         }
-        //console.log("fadeout");
         testContext.putImageData(tempImage,0,0);
-    }/*else{
-        console.log("no image", Math.random());
-    }*/
-    
-    
-    // ctx.putImageData(imgData, 90, 10);
+    }
 }
 
 //listen for mouse movement
 canvas.addEventListener("mousemove", moveLight);
 //hide the cursor on the canvas
-// canvas.style.cursor = "none";
-canvas.onclick = fadeOut;
+canvas.style.cursor = "none";
