daf9f8f72ad8928c132ea7753e12c7fa692e4193
Charles B <polyglob@yahoo.com>
2018-06-15 17:29:56 +0000

    Latest version with color changing but not exactly the way I want

--- aurora.js
+++ aurora.js
@@ -3,7 +3,7 @@ let context = canvas.getContext("2d");
 
 let testCanvas = document.querySelector("#testCanvas");
 let testContext = testCanvas.getContext("2d");
-const FADE_AMOUNT = 3;
+const FADE_AMOUNT = 1;
 
 // var offscreen = new OffscreenCanvas(256, 256);
 // var gl = offscreen.getContext('2d');
@@ -16,7 +16,15 @@ trees.src="images/trees.png"
 
 let light = new Image();
 light.src="images/light.png";
-light.setAttribute("style","background-color:#00ff00ff;");
+// light.setAttribute("style","background-color:#00ff00ff;");
+
+let testLight = document.querySelector("#testLight");
+// testLight.setAttribute("style","color: rgba(255,255,0, 0.5);")
+console.log("TEST LIGHT IS "+testLight);
+let RGB = [255,0,0];
+let currentRGBIndex = 1;
+let lastRGBIndex = 0;
+// let hueDegrees = 0;
 
 
 function bkgdLoadComplete(){
@@ -35,7 +43,13 @@ function update(){
 function moveLight(e){
     testContext.drawImage(light,e.clientX-(light.width/2),e.clientY-(light.height));
     
+  /*//DOESN WORK WITH DRAW IMAGE 
+  hueDegrees++;
+    if(hueDegrees==360){
+        hueDegrees = 0;
+    }*/
     
+    //testCanvas.setAttribute("style","filter: hue-rotate("+hueDegrees+"deg);");//background: rgba("+RGB[0]+","+RGB[1]+","+RGB[2]+",1);")
     
 //     testContext.fillStyle = "#00ff0011";
 //   testContext.fillRect(0, 0, testCanvas.width, testCanvas.height)
@@ -67,10 +81,25 @@ function updateAuroraEffect(){
         // for(let i = 3;i<tempData.length;i+=4){
         for(let i = 3;i<tempData.length;i+=4){
             tempData[i]=tempData[i]>FADE_AMOUNT?tempData[i]-FADE_AMOUNT:0;
-            tempData[i-2]+=FADE_AMOUNT;
+            tempData[i-3] = RGB[0];
+            tempData[i-2] = RGB[1];
+            tempData[i-1] = RGB[2];
+            //tempData[i-2]+=FADE_AMOUNT;
         }
         testContext.putImageData(tempImage,0,0);
     }
+   //cycle through different colors
+    if(RGB[currentRGBIndex]<255){
+        RGB[currentRGBIndex]++;
+        RGB[lastRGBIndex]--;
+    }else{
+        //move to the next color
+        lastRGBIndex = currentRGBIndex;
+        currentRGBIndex = currentRGBIndex+1<RGB.length?currentRGBIndex+1:0;
+        // console.log("nex RGB index"+currentRGBIndex);
+    }
+   // testLight.setAttribute("style","background: rgba("+RGB[0]+","+RGB[1]+","+RGB[2]+",0.5);")
+   // console.log(testLight);//"color: rgba("+RGB[0]+","+RGB[1]+","+RGB[2]+",0.5);");
 }
 
 //listen for mouse movement
